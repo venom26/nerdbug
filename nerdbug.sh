441b33c84c7776b9843453ac94de6d77657ab761
venom26 <54362015+venom26@users.noreply.github.com>
2021-06-23 17:43:47 +0530

    Update nerdbug.sh

--- nerdbug.sh
+++ nerdbug.sh
@@ -13,17 +13,17 @@ if [ -s ~/domains.txtls ]
 then
         cat domains.txtls >> alltargets.txtls
         cat domains.txtls | httpx -fl 0 -mc 200 >> newurls.txtls
-        echo "Total $(wc -l < newurls.txtls) live websites found" | notify -telegram -telegram-api-key <<enter api key>> -telegram-chat-id <<enter chat id>>
+        echo "Total $(wc -l < newurls.txtls) live websites found" | notify 
         cat alltargets.txtls | anew >> alltargets2.txtls
         rm alltargets.txtls
         mv alltargets2.txtls alltargets.txtls
-        echo Below vulnerability $(date +%F-%T) | notify -telegram -telegram-api-key <<enter api key>> -telegram-chat-id <<enter chat id>>
+        echo Below vulnerability $(date +%F-%T) | notify 
         echo "Starting nuclei"
-        cat newurls.txtls | nuclei -t /root/nuclei-templates/ -silent -severity critical,high,medium | notify -telegram -telegram-api-key <<enter api key>> -telegram-chat-id <<enter chat id>>
+        cat newurls.txtls | nuclei -t /root/nuclei-templates/ -silent -severity critical,high,medium -c 300 -H "User-Agent: Mozilla/5.0 Windows NT 10.0 Win64 AppleWebKit/537.36 Chrome/69.0.3497.100"| notify 
         echo "nuclei completed"
         rm newurls.txtls domains.txtls newdomains.md
 else
-        echo No new domains $(date +%F-%T) | notify -telegram -telegram-api-key <<enter api key>> -telegram-chat-id <<enter chat id>>
+        echo No new domains $(date +%F-%T) | notify 
         rm domains.txtls
         rm newdomains.md
 fi
